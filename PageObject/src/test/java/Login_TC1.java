6a7496ae54ec1bf15a991269d99336af1a697584
iamchandra1511 <chandra.myhome@gmail.com>
2024-02-09 22:40:05 +0530

    PageObjectModel Example

---
+++ PageObject/src/test/java/Login_TC1.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+//import Dashboard;
+//import LoginPage;
+
+public class Login_TC1 
+{
+	
+	public static void main(String[] args) throws InterruptedException 
+	{
+		
+		System.setProperty("webdriver.chrome.driver", "E:\\BrowserNatives\\chrome117\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.demoqa.com/books");
+		
+		//Creating object of home page
+		//HomePage home = new HomePage(driver);
+		
+		//Creating object of Login page
+		LoginPage login = new LoginPage(driver);
+		
+		//Creating object of Dashboard
+		Dashboard dashboard = new Dashboard(driver);
+		
+		//Click on Login button
+		//home.clickLogin();
+		
+		//Enter username & password
+		login.enterUsername("---Your Username---");
+		login.enterPassword("---Your Password---");
+		
+		//Click on login button
+		login.clickLogin();
+		Thread.sleep(3000);
+		
+		
+		//Capture the page heading and print on console
+		System.out.println("The page heading is --- " +dashboard.getHeading());
+		
+		//Click on Logout button
+		dashboard.clickLogout();
+
+               //Close browser instance
+               driver.quit();
+	}
+
+}
